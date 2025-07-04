e8e93c0341ea459dfc91e447976f58875d4d524a
Akshata Bhosale <bhosaleakshata726@gmail.com>
2025-07-04 20:13:46 +0530

    Update wrapper.java

--- wrapper.java
+++ wrapper.java
@@ -1,5 +1,14 @@
-package program;
 
-public class Program3 {
+public class Program {
+
+	public static void main(String[] args) {
+		//Converting primitive to wrapper --> Boxing 
+		int a = 10;
+		Integer b = new Integer(a); // Boxing 
+		
+		//Converting wrapper to primitive -> unboxing 
+		int c = b.intValue(); // Unboxing  
+
+	}
 
 }
